4fe0961e0271196a45396a5ba62b5ae709efc655
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2019-08-08 18:40:41 -0500

    Se crea la pagina DateTime

---
+++ src/app/pages/date-time/date-time.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DateTimePage } from './date-time.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DateTimePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [DateTimePage]
+})
+export class DateTimePageModule {}
