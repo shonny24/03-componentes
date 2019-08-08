8417d0c88b3df2561e3eb9e51e58cceb00d819dc
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2019-08-08 18:31:54 -0500

    Check finalizada

--- src/app/pages/check/check.page.ts
+++ src/app/pages/check/check.page.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckPage implements OnInit {
 
+  data =[
+    {
+      name: 'primary',
+      selected: false
+    },
+    {
+      name: 'secondary',
+      selected: true
+    },
+    {
+      name: 'tertiary',
+      selected: false
+    },
+    {
+      name: 'success',
+      selected: true
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick( check ){
+    console.log(check);
   }
 
 }
