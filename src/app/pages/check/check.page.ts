177afb82b4886f6ef0349625011b42ec5212a12f
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2019-08-08 18:01:37 -0500

    Se crea la pagina Check

---
+++ src/app/pages/check/check.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.page.html',
+  styleUrls: ['./check.page.scss'],
+})
+export class CheckPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
