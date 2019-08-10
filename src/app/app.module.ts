7be78da06ed7a41f2c1db9d3189ed070df118bf6
Jhonny Ocampo <jhonnyokmpo24@hotmail.com>
2019-08-10 17:37:21 -0500

    Page list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
